fdfb0e96731e3a2504677c85abd91bdfe4788123
leizhiping <leizhiping@junction-tech.com>
2013-03-09 18:14:42 +0800

    add qt project

---
+++ cocos2dx/platform/qt/CCApplication.cpp
@@ -0,0 +1,151 @@
+/****************************************************************************
+ Copyright (c) 2010 cocos2d-x.org
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#import "CCApplication.h"
+#include <algorithm>
+
+#include "CCGeometry.h"
+#include "CCDirector.h"
+#import "CCDirectorCaller.h"
+
+NS_CC_BEGIN
+
+CCApplication* CCApplication::sm_pSharedApplication = 0;
+
+CCApplication::CCApplication()
+{
+    CCAssert(! sm_pSharedApplication, "sm_pSharedApplication already exist");
+    sm_pSharedApplication = this;
+}
+
+CCApplication::CCApplication(int& argc, char** argv):
+    QApplication(argc, argv)
+{
+    CCAssert(! sm_pSharedApplication, "sm_pSharedApplication already exist");
+    sm_pSharedApplication = this;
+}
+
+CCApplication::~CCApplication()
+{
+    CCAssert(this == sm_pSharedApplication, "sm_pSharedApplication != this");
+    sm_pSharedApplication = 0;
+}
+
+int CCApplication::run()
+{
+    if (/*initInstance() &&*/ applicationDidFinishLaunching()) 
+    {
+
+        CCDirectorCaller::sharedDirectorCaller()->startMainLoop();
+        //[[CCDirectorCaller sharedDirectorCaller] startMainLoop];
+    }
+    return 0;
+}
+
+void CCApplication::setAnimationInterval(double interval)
+{
+    CCDirectorCaller::sharedDirectorCaller()->setAnimationInterval(interval);
+   // [[CCDirectorCaller sharedDirectorCaller] setAnimationInterval: interval ];
+}
+
+TargetPlatform CCApplication::getTargetPlatform()
+{
+    return kTargetMacOS;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// static member function
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+CCApplication* CCApplication::sharedApplication()
+{
+    CCAssert(sm_pSharedApplication, "sm_pSharedApplication not set");
+    return sm_pSharedApplication;
+}
+
+ccLanguageType CCApplication::getCurrentLanguage()
+{
+    // get the current language and country config
+
+    // get the current language code.(such as English is "en", Chinese is "zh" and so on)
+
+    QString lang = "zh";
+
+    if (lang == "zh")
+    {
+        return kLanguageChinese;
+    }
+    else if (lang == "fr")
+    {
+        return kLanguageFrench;
+    }
+    else if (lang == "it")
+    {
+        return kLanguageItalian;
+    }
+    else if (lang == "de")
+    {
+        return kLanguageGerman;
+    }
+    else if (lang == "es")
+    {
+        return kLanguageSpanish;
+    }
+    else if (lang == "ru")
+    {
+        return kLanguageRussian;
+    }
+
+    return kLanguageEnglish;
+    
+    return ret;
+}
+
+void CCApplication::setResourceRootPath(const std::string& rootResDir)
+{
+    m_resourceRootPath = rootResDir;
+    std::replace(m_resourceRootPath.begin(), m_resourceRootPath.end(), '\\', '/');
+    if (m_resourceRootPath[m_resourceRootPath.length() - 1] != '/')
+    {
+        m_resourceRootPath += '/';
+    }
+}
+
+const std::string& CCApplication::getResourceRootPath(void)
+{
+    return m_resourceRootPath;
+}
+
+void CCApplication::setStartupScriptFilename(const std::string& startupScriptFile)
+{
+    m_startupScriptFilename = startupScriptFile;
+    std::replace(m_startupScriptFilename.begin(), m_startupScriptFilename.end(), '\\', '/');
+}
+
+const std::string& CCApplication::getStartupScriptFilename(void)
+{
+    return m_startupScriptFilename;
+}
+
+NS_CC_END
---
+++ cocos2dx/proj.qt/platform.pri
@@ -0,0 +1,55 @@
+HEADERS += \
+    ../platform/platform.h \
+    ../platform/CCThread.h \
+    ../platform/CCStdC.h \
+    ../platform/CCSAXParser.h \
+    ../platform/CCPlatformMacros.h \
+    ../platform/CCPlatformConfig.h \
+    ../platform/CCLibxml2.h \
+    ../platform/CCImage.h \
+    ../platform/CCGL.h \
+    ../platform/CCFileUtils.h \
+    ../platform/CCEGLView_platform.h \
+    ../platform/CCCommon.h \
+    ../platform/CCArchOptimalParticleSystem.h \
+    ../platform/CCApplication_platform.h \
+    ../platform/CCAccelerometer_platform.h \
+    ../platform/CCImageCommon_cpp.h \
+    ../platform/CCAccelerometerDelegate.h \
+    ../platform/CCEGLViewProtocol.h \
+    ../platform/CCFileUtilsCommon_cpp.h \
+    ../platform/CCApplicationProtocol.h \
+    ../platform/qt/CCGLWidget.h \
+    ../platform/qt/CCEGLView.h
+
+HEADERS += \
+    ../platform/qt/EAGLView.h \
+    ../platform/qt/CCGL.h \
+    ../platform/qt/CCStdC.h \
+    ../platform/qt/CCPlatformDefine.h \
+    ../platform/qt/CCDirectorCaller.h \
+    ../platform/qt/CCApplication.h \
+    ../platform/qt/CCEventDispatcher.h \
+    ../platform/qt/CCAccelerometer.h
+
+SOURCES += \
+    ../platform/platform.cpp \
+    ../platform/CCThread.cpp \
+    ../platform/CCStdC.cpp \
+    ../platform/CCSAXParser.cpp \
+    ../platform/CCImage.cpp \
+    ../platform/CCGL.cpp \
+    ../platform/CCFileUtils.cpp \
+    ../platform/CCCommon.cpp \
+    ../platform/CCEGLViewProtocol.cpp \
+    ../platform/qt/CCGLWidget.cpp
+
+SOURCES += \
+    ../platform/qt/EAGLView.cpp \
+    ../platform/qt/CCApplication.cpp \
+    ../platform/qt/CCEGLView.cpp \
+    ../platform/qt/CCCommon.cpp \
+    ../platform/qt/CCImage.cpp \
+    ../platform/qt/CCFileUtils.cpp \
+    ../platform/qt/CCEventDispatcher.cpp \
+    ../platform/qt/CCDirectorCaller.cpp
