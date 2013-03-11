0226b9ef90b1c9e337150b5fb72d274c82ef4190
erduoerduo <94069625@qq.com>
2013-03-11 11:45:28 +0800

    cocos2d-x中主要的类或者说对象

--- document/Groups.h
+++ document/Groups.h
@@ -1,25 +1,25 @@
 /**
-@defgroup actions           Actions
-@defgroup data_structures   Data Structures
-@defgroup base_nodes        Base Nodes
-@defgroup effects           Effects
-@defgroup cocosbuilder      CocosBuilder Support
-@defgroup global            Global
-@defgroup input             Human Machine Interation
-@defgroup data_storage      Data Storage
-@defgroup layer             Layer
-@defgroup scene             Scene
-@defgroup transition        Transition
-@defgroup GUI               GUI
-@defgroup label             Label
-@defgroup menu              Menu, MenuItem
-@defgroup control_extension Control Exntension
-@defgroup misc_nodes        Misc Nodes
-@defgroup particle_nodes    Particle System
-@defgroup platform          Platform Adaptor
-@defgroup script_support    Script Support
-@defgroup shaders           Shaders
-@defgroup sprite_nodes      Sprite Nodes        
-@defgroup textures          Textures
-@defgroup tilemap_parallax_nodes    TileMap, Parallax
+@defgroup actions           Actions                       动作
+@defgroup data_structures   Data Structures               数据结构
+@defgroup base_nodes        Base Nodes                    基节点
+@defgroup effects           Effects                       效果
+@defgroup cocosbuilder      CocosBuilder Support          编辑支持
+@defgroup global            Global                        全局输入
+@defgroup input             Human Machine Interation      输入
+@defgroup data_storage      Data Storage                  数据存储
+@defgroup layer             Layer                         层
+@defgroup scene             Scene                         场景
+@defgroup transition        Transition                    过渡
+@defgroup GUI               GUI                           用户界面
+@defgroup label             Label                         标签
+@defgroup menu              Menu, MenuItem                菜单，菜单项
+@defgroup control_extension Control Extension             控制扩展
+@defgroup misc_nodes        Misc Nodes                    其他节点
+@defgroup particle_nodes    Particle System               粒子系统
+@defgroup platform          Platform Adaptor              平台
+@defgroup script_support    Script Support                脚本支持
+@defgroup shaders           Shaders                       着色
+@defgroup sprite_nodes      Sprite Nodes                  精灵节点
+@defgroup textures          Textures                      纹理
+@defgroup tilemap_parallax_nodes    TileMap, Parallax     瓦片地图 视差
  */
