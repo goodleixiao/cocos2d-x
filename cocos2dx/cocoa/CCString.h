021564183380290b4e0b7c566933a28181927ebb
erduoerduo <94069625@qq.com>
2013-03-13 13:53:17 +0800

    字符串

--- cocos2dx/cocoa/CCString.h
+++ cocos2dx/cocoa/CCString.h
@@ -36,7 +36,7 @@ THE SOFTWARE.
 NS_CC_BEGIN
 
 /**
- * @addtogroup data_structures
+ * @addtogroup data_structures      数据结构
  * @{
  */
 
@@ -51,42 +51,57 @@ public:
     virtual ~CCString();
     
     /* override assignment operator */
+    /* 重载分配操作 */
     CCString& operator= (const CCString& other);
 
     /** init a string with format, it's similar with the c function 'sprintf' */ 
+    /** 用格式方式来初始化一个字符串，像c中的打印函数 */
     bool initWithFormat(const char* format, ...);
 
     /** convert to int value */
+    /** 转成整数值 */
     int intValue() const;
 
     /** convert to unsigned int value */
+    /** 转成无符号整数值 */
     unsigned int uintValue() const;
 
     /** convert to float value */
+    /** 转成浮点值 */
     float floatValue() const;
 
     /** convert to double value */
+    /** 转成双精度浮点值 */
     double doubleValue() const;
 
     /** convert to bool value */
+    /** 转成布尔值 */
     bool boolValue() const;
 
     /** get the C string */
+    /** 获取c字符串 */
     const char* getCString() const;
 
     /** get the length of string */
+    /** 获取字符串长度 */
     unsigned int length() const;
 
     /** compare to a c string */
+    /** 比较一个c字符串 */
     int compare(const char *) const;
 
     /* override functions */
+    /* 重载 */
     virtual CCObject* copyWithZone(CCZone* pZone);
     virtual bool isEqual(const CCObject* pObject);
 
     /** create a string with std string, you can also pass a c string pointer because the default constructor of std::string can access a c string pointer. 
      *  @return A CCString pointer which is an autorelease object pointer,
      *          it means that you needn't do a release operation unless you retain it.
+     */
+    /** 创建一个字符串，使用标准字符串，可以使用c字符串的指针作为参数
+     * 返回一个自动释放的指针，字符串
+     * 不用手动释放
      */
     static CCString* create(const std::string& str);
 
@@ -95,11 +110,19 @@ public:
      *  @return A CCString pointer which is an autorelease object pointer,
      *          it means that you needn't do a release operation unless you retain it.
      */ 
+    /** 创建一个字符串，使用格式化方式
+     * 返回一个自动释放的指针，字符串
+     * 不用手动释放
+     */
     static CCString* createWithFormat(const char* format, ...);
 
     /** create a string with binary data 
      *  @return A CCString pointer which is an autorelease object pointer,
      *          it means that you needn't do a release operation unless you retain it.
+     */
+    /** 创建一个字符串，使用字节数据
+     * 返回一个自动释放的指针，字符串
+     * 不用手动释放
      */
     static CCString* createWithData(const unsigned char* pData, unsigned long nLen);
 
@@ -107,11 +130,16 @@ public:
      *  @return A CCString pointer which is an autorelease object pointer,
      *          it means that you needn't do a release operation unless you retain it.
      */
+    /** 创建一个字符串，使用一个文件
+     * 返回一个自动释放的指针，字符串
+     * 不用手动释放
+     */
     static CCString* createWithContentsOfFile(const char* pszFileName);
 
 private:
 
     /** only for internal use */
+    /** 仅在内部使用 */
     bool initWithFormatAndValist(const char* format, va_list ap);
 
 public:
