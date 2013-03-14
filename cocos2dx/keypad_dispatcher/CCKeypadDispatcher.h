4050c26380c3103dd00f46afae72702c303bd16d
erduoerduo <94069625@qq.com>
2013-03-14 19:23:53 +0800

    键盘调度

--- cocos2dx/keypad_dispatcher/CCKeypadDispatcher.h
+++ cocos2dx/keypad_dispatcher/CCKeypadDispatcher.h
@@ -31,12 +31,13 @@ THE SOFTWARE.
 NS_CC_BEGIN
 
 /**
- * @addtogroup input
+ * @addtogroup input    输入
  * @{
  */
 
 typedef enum {
     // the back key clicked msg
+    // 返回键点击信息
     kTypeBackClicked = 1,
     kTypeMenuClicked,
 } ccKeypadMSGType;
@@ -46,6 +47,7 @@ struct _ccCArray;
 @class CCKeypadDispatcher
 @brief Dispatch the keypad message from the phone
 */
+// 处理键盘消息
 class CC_DLL CCKeypadDispatcher : public CCObject
 {
 public:
@@ -55,26 +57,31 @@ public:
     /**
     @brief add delegate to concern keypad msg
     */
+    // 增加一个委托，关注键盘消息
     void addDelegate(CCKeypadDelegate* pDelegate);
 
     /**
     @brief remove the delegate from the delegates who concern keypad msg
     */
+    // 移除一个委托，使用委托为参数
     void removeDelegate(CCKeypadDelegate* pDelegate);
 
     /**
     @brief force add the delegate
     */
+    // 强制增加一个委托
     void forceAddDelegate(CCKeypadDelegate* pDelegate);
 
     /**
     @brief force remove the delegate
     */
+    // 强制移除一个委托
     void forceRemoveDelegate(CCKeypadDelegate* pDelegate);
 
     /**
     @brief dispatch the key pad msg
     */
+    // 调度键盘消息
     bool dispatchKeypadMSG(ccKeypadMSGType nMsgType);
 
 protected:
