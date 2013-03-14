e5ea81a44cf7d64249e6d9189af75acb68354c40
erduoerduo <94069625@qq.com>
2013-03-14 11:30:00 +0800

    网格动作

--- cocos2dx/actions/CCActionGrid.h
+++ cocos2dx/actions/CCActionGrid.h
@@ -33,11 +33,12 @@ NS_CC_BEGIN
 class CCGridBase;
 
 /**
- * @addtogroup actions
+ * @addtogroup actions      动作
  * @{
  */
 
 /** @brief Base class for Grid actions */
+// 网格动作
 class CC_DLL CCGridAction : public CCActionInterval
 {
 public:
@@ -46,9 +47,11 @@ public:
     virtual CCActionInterval* reverse(void);
 
     /** initializes the action with size and duration */
+    // 使用大小和间隔初始化
     virtual bool initWithDuration(float duration, const CCSize& gridSize);
 
     /** returns the grid */
+    // 返回网格
     virtual CCGridBase* getGrid(void);
 
 public:
@@ -58,6 +61,7 @@ public:
     //CC_DEPRECATED_ATTRIBUTE static CCGridAction* create(const CCSize& gridSize, float duration);
 
     /** creates the action with size and duration */
+    // 创建一个动作，使用大小和间隔；以前版本没有此方法
     static CCGridAction* create(float duration, const CCSize& gridSize);
 protected:
     CCSize m_sGridSize;
@@ -67,48 +71,62 @@ protected:
  @brief Base class for CCGrid3D actions.
  Grid3D actions can modify a non-tiled grid.
  */
+// 3D网格动作，可以修改网格
 class CC_DLL CCGrid3DAction : public CCGridAction
 {
 public:
     /** returns the grid */
+    // 返回网格
     virtual CCGridBase* getGrid(void);
     /** returns the vertex than belongs to certain position in the grid */
+    // 返回网格顶点
     ccVertex3F vertex(const CCPoint& position);
     /** returns the non-transformed vertex than belongs to certain position in the grid */
+    // 返回非转化顶点，
     ccVertex3F originalVertex(const CCPoint& position);
     /** sets a new vertex to a certain position of the grid */
+    // 设置一个新的顶点
     void setVertex(const CCPoint& position, const ccVertex3F& vertex);
 
 public:
     /** creates the action with size and duration */
+    // 创建一个动作用大小和间隔
     static CCGrid3DAction* create(float duration, const CCSize& gridSize);
 };
 
 /** @brief Base class for CCTiledGrid3D actions */
+// 动作
 class CC_DLL CCTiledGrid3DAction : public CCGridAction
 {
 public:
     /** returns the tile that belongs to a certain position of the grid */
+    // 返回瓦片，属于网格指定位置
     ccQuad3 tile(const CCPoint& position);
     /** returns the non-transformed tile that belongs to a certain position of the grid */
+    // 返回非转化瓦片
     ccQuad3 originalTile(const CCPoint& position);
     /** sets a new tile to a certain position of the grid */
+    // 设置新的瓦片，在指定位置
     void setTile(const CCPoint& position, const ccQuad3& coords);
 
     /** returns the grid */
+    // 返回网格
     virtual CCGridBase* getGrid(void);
 
 public:
     /** creates the action with size and duration */
+    // 创建动作，使用大小和间隔
     static CCTiledGrid3DAction* create(float duration, const CCSize& gridSize);
 };
 
 /** @brief CCAccelDeccelAmplitude action */
+// 幅度 动作
 class CC_DLL CCAccelDeccelAmplitude : public CCActionInterval
 {
 public:
     virtual ~CCAccelDeccelAmplitude(void);
     /** initializes the action with an inner action that has the amplitude property, and a duration time */
+    // 初始化动作使用幅度属性和间隔
     bool initWithAction(CCAction *pAction, float duration);
 
     virtual void startWithTarget(CCNode *pTarget);
@@ -116,12 +134,15 @@ public:
     virtual CCActionInterval* reverse(void);
 
     /** get amplitude rate */
+    // 获取幅度比率
     inline float getRate(void) { return m_fRate; }
     /** set amplitude rate */
+    // 设置幅度比率
     inline void setRate(float fRate) { m_fRate = fRate; }
 
 public:
     /** creates the action with an inner action that has the amplitude property, and a duration time */
+    // 初始化动作使用幅度属性和间隔
     static CCAccelDeccelAmplitude* create(CCAction *pAction, float duration);
 
 protected:
@@ -130,16 +151,20 @@ protected:
 };
 
 /** @brief CCAccelAmplitude action */
+// 加速幅度动作
 class CC_DLL CCAccelAmplitude : public CCActionInterval
 {
 public:
     ~CCAccelAmplitude(void);
     /** initializes the action with an inner action that has the amplitude property, and a duration time */
+    // 初始化动作使用幅度属性和间隔
     bool initWithAction(CCAction *pAction, float duration);
 
     /** get amplitude rate */
+    // 获取幅度比率
     inline float getRate(void) { return m_fRate; }
     /** set amplitude rate */
+    // 设置幅度
     inline void setRate(float fRate) { m_fRate = fRate; }
 
     virtual void startWithTarget(CCNode *pTarget);
@@ -148,6 +173,7 @@ public:
 
 public:
     /** creates the action with an inner action that has the amplitude property, and a duration time */
+    // 初始化动作使用幅度属性和间隔
     static CCAccelAmplitude* create(CCAction *pAction, float duration);
 protected:
     float m_fRate;
@@ -155,16 +181,20 @@ protected:
 };
 
 /** @brief CCDeccelAmplitude action */
+// 减速幅度
 class CC_DLL CCDeccelAmplitude : public CCActionInterval
 {
 public:
     ~CCDeccelAmplitude(void);
     /** initializes the action with an inner action that has the amplitude property, and a duration time */
+    // 初始化动作使用幅度属性和间隔
     bool initWithAction(CCAction *pAction, float duration);
 
     /** get amplitude rate */
+    // 获取幅度比率
     inline float getRate(void) { return m_fRate; }
     /** set amplitude rate */
+    // 设置幅度比率
     inline void setRate(float fRate) { m_fRate = fRate; }
 
     virtual void startWithTarget(CCNode *pTarget);
@@ -173,6 +203,7 @@ public:
 
 public:
     /** creates the action with an inner action that has the amplitude property, and a duration time */
+    // 创建动作使用幅度属性和间隔
     static CCDeccelAmplitude* create(CCAction *pAction, float duration);
 
 protected:
@@ -185,6 +216,8 @@ protected:
  Call if you want to remove the the grid effect. Example:
  CCSequence::actions(Lens::action(...), CCStopGrid::action(...), NULL);
  */
+// 停止网格动作，当网络激活状态不要调用动作。如果你想移除网格效果，调用；如
+//  CCSequence::actions(Lens::action(...), CCStopGrid::action(...), NULL);
 class CC_DLL CCStopGrid : public CCActionInstant
 {
 public:
@@ -192,20 +225,24 @@ public:
 
 public:
     /** Allocates and initializes the action */
+    // 分配和初始化动作
     static CCStopGrid* create(void);
 };
 
 /** @brief CCReuseGrid action */
+// 重用动作网格
 class CC_DLL CCReuseGrid : public CCActionInstant
 {
 public:
     /** initializes an action with the number of times that the current grid will be reused */
+    // 初始化动作，重用次数
     bool initWithTimes(int times);
 
     virtual void startWithTarget(CCNode *pTarget);
 
 public:
     /** creates an action with the number of times that the current grid will be reused */
+    // 创建一个动作，使用重用次数
     static CCReuseGrid* create(int times);
 protected:
     int m_nTimes;
