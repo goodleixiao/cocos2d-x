ef75f6b7bc1e9725ab7e651b4e68b41cf83316ee
erduoerduo <94069625@qq.com>
2013-03-15 13:26:42 +0800

    触摸的处理程序

--- cocos2dx/touch_dispatcher/CCTouchHandler.h
+++ cocos2dx/touch_dispatcher/CCTouchHandler.h
@@ -34,7 +34,7 @@ THE SOFTWARE.
 NS_CC_BEGIN
 
 /**
- * @addtogroup input
+ * @addtogroup input  输入
  * @{
  */
 
@@ -42,28 +42,34 @@ NS_CC_BEGIN
  CCTouchHandler
  Object than contains the delegate and priority of the event handler.
 */
+// 每个处理包含委托和优先级
 class CC_DLL  CCTouchHandler : public CCObject
 {
 public:
     virtual ~CCTouchHandler(void);
 
     /** delegate */
+    // 委托
     CCTouchDelegate* getDelegate();
     void setDelegate(CCTouchDelegate *pDelegate);
 
     /** priority */
+    // 优先级
     int getPriority(void);
     void setPriority(int nPriority);
 
     /** enabled selectors */
+    // 启用选择器
     int getEnabledSelectors(void);
     void setEnalbedSelectors(int nValue);
 
     /** initializes a TouchHandler with a delegate and a priority */
+    // 初始化处理程序，使用委托和优先级为参数
     virtual bool initWithDelegate(CCTouchDelegate *pDelegate, int nPriority);
 
 public:
     /** allocates a TouchHandler with a delegate and a priority */
+    // 分配一个处理程序，使用委托和优先级为参数
     static CCTouchHandler* handlerWithDelegate(CCTouchDelegate *pDelegate, int nPriority);
 
 protected:
@@ -75,14 +81,17 @@ protected:
 /** CCStandardTouchHandler
  It forwards each event to the delegate.
  */
+// 标准委托
 class CC_DLL  CCStandardTouchHandler : public CCTouchHandler
 {
 public:
     /** initializes a TouchHandler with a delegate and a priority */
+    // 初始化处理程序，使用委托和优先级为参数
     virtual bool initWithDelegate(CCTouchDelegate *pDelegate, int nPriority);
 
 public:
     /** allocates a TouchHandler with a delegate and a priority */
+    // 分配一个处理程序，使用委托和优先级为参数
     static CCStandardTouchHandler* handlerWithDelegate(CCTouchDelegate *pDelegate, int nPriority);
 };
 
@@ -91,23 +100,28 @@ public:
  Object than contains the claimed touches and if it swallows touches.
  Used internally by TouchDispatcher
  */
+// 目标委托。 对象要求触摸有触感
 class CC_DLL  CCTargetedTouchHandler : public CCTouchHandler
 {
 public:
     ~CCTargetedTouchHandler(void);
 
     /** whether or not the touches are swallowed */
+    // 是否有触感
     bool isSwallowsTouches(void);
     void setSwallowsTouches(bool bSwallowsTouches);
 
     /** MutableSet that contains the claimed touches */
+    // 返回多个触摸
     CCSet* getClaimedTouches(void);
 
     /** initializes a TargetedTouchHandler with a delegate, a priority and whether or not it swallows touches or not */
+    // 初始化处理程序，使用委托，优先级，是否有吞噬为参数
     bool initWithDelegate(CCTouchDelegate *pDelegate, int nPriority, bool bSwallow);
 
 public:
     /** allocates a TargetedTouchHandler with a delegate, a priority and whether or not it swallows touches or not */
+    //  分配处理程序，使用委托，优先级，是否有吞噬为参数
     static CCTargetedTouchHandler* handlerWithDelegate(CCTouchDelegate *pDelegate, int nPriority, bool bSwallow);
 
 protected:
