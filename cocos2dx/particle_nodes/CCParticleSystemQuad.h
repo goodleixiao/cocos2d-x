3b388dbe24608925dd6a9d2f9eb1a621e4324baa
erduoerduo <94069625@qq.com>
2013-03-15 12:32:23 +0800

    限定的粒子系统类

--- cocos2dx/particle_nodes/CCParticleSystemQuad.h
+++ cocos2dx/particle_nodes/CCParticleSystemQuad.h
@@ -50,11 +50,12 @@ Special features and Limitations:
 - It supports batched rendering since 1.1
 @since v0.8
 */
+// 是粒子系统子类，具有所有粒子系统的特性；特殊和限制为：粒子为任意浮点数；可以缩放，粒子可以旋转，支持渲染
 class CC_DLL CCParticleSystemQuad : public CCParticleSystem
 {
 protected:
     ccV3F_C4B_T2F_Quad    *m_pQuads;        // quads to be rendered
-    GLushort            *m_pIndices;    // indices
+    GLushort            *m_pIndices;    // indices      指数
 
 #if CC_TEXTURE_ATLAS_USE_VAO
     GLuint                m_uVAOname;
@@ -69,25 +70,31 @@ public:
     /** creates an initializes a CCParticleSystemQuad from a plist file.
     This plist files can be created manually or with Particle Designer:  
     */
+    // 使用文件创建；文件可以手动创建或粒子设计
     static CCParticleSystemQuad * create(const char *plistFile);
 
     /** initializes the indices for the vertices*/
+    // 初始化指数，为顶点
     void initIndices();
 
     /** initializes the texture with a rectangle measured Points */
+    // 初始化纹理，使用矩形为参数
     void initTexCoordsWithRect(const CCRect& rect);
 
     /** Sets a new CCSpriteFrame as particle.
     WARNING: this method is experimental. Use setTexture:withRect instead.
     @since v0.99.4
     */
+    // 设置新的精灵帧，作为粒子；用于测试； 使用setTexture:withRect替换
     void setDisplayFrame(CCSpriteFrame *spriteFrame);
 
     /** Sets a new texture with a rect. The rect is in Points.
     @since v0.99.4
     */
+    // 设置新的纹理，使用矩形，以点为单位
     void setTextureWithRect(CCTexture2D *texture, const CCRect& rect);
     // super methods
+    // 父类方法
     virtual bool initWithTotalParticles(unsigned int numberOfParticles);
     virtual void setTexture(CCTexture2D* texture);
     virtual void updateQuadWithParticle(tCCParticle* particle, const CCPoint& newPosition);
@@ -98,6 +105,7 @@ public:
     
     /** listen the event that coming to foreground on Android
      */
+    // android平台监听事件，进入后台
     void listenBackToForeground(CCObject *obj);
 
     static CCParticleSystemQuad * create();
