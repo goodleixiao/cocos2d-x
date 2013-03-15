1ecc09838f421c87bde87be29cd715595be2eced
erduoerduo <94069625@qq.com>
2013-03-15 08:27:15 +0800

    层

--- cocos2dx/layers_scenes_transitions_nodes/CCLayer.h
+++ cocos2dx/layers_scenes_transitions_nodes/CCLayer.h
@@ -42,7 +42,7 @@ typedef enum {
 } ccTouchesMode;
 
 /**
- * @addtogroup layer
+ * @addtogroup layer	层
  * @{
  */
 
@@ -57,6 +57,9 @@ All features from CCNode are valid, plus the following new features:
 - It can receive iPhone Touches
 - It can receive Accelerometer input
 */
+/** 是节点的子类，实现触摸协议
+ * 具有所有节点特性，增加了新的特性：接收触摸和加速计输入
+ */
 class CC_DLL CCLayer : public CCNode, public CCTouchDelegate, public CCAccelerometerDelegate, public CCKeypadDelegate
 {
 public:
@@ -65,6 +68,7 @@ public:
     virtual bool init();
     
     /** create one layer */
+    // 创建一个层
     static CCLayer *create(void);
 
     virtual void onEnter();
@@ -72,12 +76,14 @@ public:
     virtual void onEnterTransitionDidFinish();
     
     // default implements are used to call script callback if exist
+    // 默认实现的回调函数
     virtual bool ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent);
     virtual void ccTouchMoved(CCTouch *pTouch, CCEvent *pEvent);
     virtual void ccTouchEnded(CCTouch *pTouch, CCEvent *pEvent);
     virtual void ccTouchCancelled(CCTouch *pTouch, CCEvent *pEvent);
 
     // default implements are used to call script callback if exist
+    // 默认回调
     virtual void ccTouchesBegan(CCSet *pTouches, CCEvent *pEvent);
     virtual void ccTouchesMoved(CCSet *pTouches, CCEvent *pEvent);
     virtual void ccTouchesEnded(CCSet *pTouches, CCEvent *pEvent);
@@ -97,11 +103,14 @@ public:
     }
     @since v0.8.0
     */
+    // 是否启用触摸；启用，层接收触摸事件
     virtual void registerWithTouchDispatcher(void);
     
     /** Register script touch events handler */
+    // 注册触摸事件处理脚本
     virtual void registerScriptTouchHandler(int nHandler, bool bIsMultiTouches = false, int nPriority = INT_MIN, bool bSwallowsTouches = false);
     /** Unregister script touch events handler */
+    // 取消注册
     virtual void unregisterScriptTouchHandler(void);
 
     /** whether or not it will receive Touch events.
@@ -109,6 +118,7 @@ public:
     Only the touches of this node will be affected. This "method" is not propagated to it's children.
     @since v0.8.1
     */
+    // 是否接收触摸
     virtual bool isTouchEnabled();
     virtual void setTouchEnabled(bool value);
     
@@ -116,6 +126,7 @@ public:
     virtual int getTouchMode();
     
     /** priority of the touch events. Default is 0 */
+    // 触摸事件优先级，默认为0
     virtual void setTouchPriority(int priority);
     virtual int getTouchPriority();
 
@@ -123,6 +134,7 @@ public:
     You can enable / disable accelerometer events with this property.
     @since v0.8.1
     */
+    // 是否接收加速计事件；可以改变其属性
     virtual bool isAccelerometerEnabled();
     virtual void setAccelerometerEnabled(bool value);
     virtual void setAccelerometerInterval(double interval);
@@ -131,12 +143,15 @@ public:
     You can enable / disable accelerometer events with this property.
     it's new in cocos2d-x
     */
+    // 是否接收键盘事件
     virtual bool isKeypadEnabled();
     virtual void setKeypadEnabled(bool value);
 
     /** Register keypad events handler */
+    // 注册键盘处理
     void registerScriptKeypadHandler(int nHandler);
     /** Unregister keypad events handler */
+    // 取消注册
     void unregisterScriptKeypadHandler(void);
 
     virtual void keyBackClicked(void);
@@ -152,6 +167,7 @@ protected:
     
 private:
     // Script touch events handler
+    // 触摸事件处理脚本
     CCTouchScriptHandlerEntry* m_pScriptTouchHandlerEntry;
     CCScriptHandlerEntry* m_pScriptKeypadHandlerEntry;
     CCScriptHandlerEntry* m_pScriptAccelerateHandlerEntry;
@@ -175,6 +191,8 @@ private:
  - RGB colors
  @since 2.1
  */
+// 是层的子类，实现颜色协议；使用实心颜色作为背景色
+// 具有层的所有特性，增加了：透明度，颜色值的属性
 class CC_DLL CCLayerRGBA : public CCLayer, public CCRGBAProtocol
 {
 public:
@@ -216,6 +234,7 @@ All features from CCLayer are valid, plus the following new features:
 - opacity
 - RGB colors
 */
+// 是层的子类，实现颜色协议；具有层的所有特性，增加了：透明度和颜色值
 class CC_DLL CCLayerColor : public CCLayerRGBA, public CCBlendProtocol
 {
 protected:
@@ -232,26 +251,34 @@ public:
     static CCLayerColor* create();
     
     /** creates a CCLayer with color, width and height in Points */
+    // 创建层，使用颜色，宽度，高度为参数
     static CCLayerColor * create(const ccColor4B& color, GLfloat width, GLfloat height);
     /** creates a CCLayer with color. Width and height are the window size. */
+    // 创建层，使用颜色，窗口的宽和高为参数
     static CCLayerColor * create(const ccColor4B& color);
 
     virtual bool init();
     /** initializes a CCLayer with color, width and height in Points */
+    // 初始化层，使用颜色，宽，高为参数
     virtual bool initWithColor(const ccColor4B& color, GLfloat width, GLfloat height);
     /** initializes a CCLayer with color. Width and height are the window size. */
+    // 创建层，使用颜色，窗口的宽和高为参数
     virtual bool initWithColor(const ccColor4B& color);
 
     /** change width in Points*/
+    // 改变宽度，以点为单位
     void changeWidth(GLfloat w);
     /** change height in Points*/
+    // 改变高度，以点为单位
     void changeHeight(GLfloat h);
     /** change width and height in Points
     @since v0.8
     */
+    // 改变高和宽，以点为单位
     void changeWidthAndHeight(GLfloat w ,GLfloat h);
 
     /** BlendFunction. Conforms to CCBlendProtocol protocol */
+    // 符合混合协议
     CC_PROPERTY(ccBlendFunc, m_tBlendFunc, BlendFunc)
 
     virtual void setOpacityModifyRGB(bool bValue) {CC_UNUSED_PARAM(bValue);}
@@ -285,21 +312,33 @@ If ' compressedInterpolation' is enabled (default mode) you will see both the st
 
 @since v0.99.5
 */
+/** 梯度层
+ * 是颜色层的子类，用于背景
+ * 具有所有颜色层的特性，增加了新的特性：方向，最终颜色，插补模式
+ * 
+ * 颜色是插补开始颜色和结束颜色，给定一个矢量。没有矢量，默认为(0,-1)
+ * 
+ * 启用compressedInterpolation，你会看到颜色的渐变过程
+ */
 class CC_DLL CCLayerGradient : public CCLayerColor
 {
 public:
 
     /** Creates a full-screen CCLayer with a gradient between start and end. */
+    // 创建层，使用开始，结束色为参数
     static CCLayerGradient* create(const ccColor4B& start, const ccColor4B& end);
 
     /** Creates a full-screen CCLayer with a gradient between start and end in the direction of v. */
+    // 创建一个满屏层，使用一个梯度在开始和结束色，方向为参数
     static CCLayerGradient* create(const ccColor4B& start, const ccColor4B& end, const CCPoint& v);
 
     virtual bool init();
     /** Initializes the CCLayer with a gradient between start and end. */
+    // 创建层，使用开始，结束色为参数
     virtual bool initWithColor(const ccColor4B& start, const ccColor4B& end);
 
     /** Initializes the CCLayer with a gradient between start and end in the direction of v. */
+    // 创建一个满屏层，使用一个梯度在开始和结束色，方向为参数
     virtual bool initWithColor(const ccColor4B& start, const ccColor4B& end, const CCPoint& v);
 
     CC_PROPERTY_PASS_BY_REF(ccColor3B, m_startColor, StartColor)
@@ -311,6 +350,7 @@ public:
     /** Whether or not the interpolation will be compressed in order to display all the colors of the gradient both in canonical and non canonical vectors
     Default: YES
     */
+    // 默认为启用插补
 protected:
     bool m_bCompressedInterpolation;
 public:
@@ -329,6 +369,7 @@ Features:
 - It supports one or more children
 - Only one children will be active a time
 */
+// 是层的子类，具有混合子对象的能力：支持一个或多个子对象；某一时刻只有一个子对象是激活的
 class CC_DLL CCLayerMultiplex : public CCLayer
 {
 protected:
@@ -343,20 +384,24 @@ public:
     /** creates a CCMultiplexLayer with an array of layers.
      @since v2.1
      */
+    // 创建层，使用层数组
     static CCLayerMultiplex* createWithArray(CCArray* arrayOfLayers);
 
     /** creates a CCLayerMultiplex with one or more layers using a variable argument list. */
+    // 创建层，使用一个或多个层为参数
     static CCLayerMultiplex * create(CCLayer* layer, ... );
 
     /**
      * lua script can not init with undetermined number of variables
      * so add these functions to be used with lua.
      */
+    // 创建层；
     static CCLayerMultiplex * createWithLayer(CCLayer* layer);
 
     void addLayer(CCLayer* layer);
 
     /** initializes a MultiplexLayer with one or more layers using a variable argument list. */
+    // 初始化层，使用一个或多个层为参数
     bool initWithLayers(CCLayer* layer, va_list params);
     /** switches to a certain layer indexed by n. 
     The current (old) layer will be removed from it's parent with 'cleanup:YES'.
@@ -365,12 +410,14 @@ public:
     /** initializes a CCMultiplexLayer with an array of layers
     @since v2.1
     */
+    // 使用数组来初始化
     bool initWithArray(CCArray* arrayOfLayers);
 
     void switchTo(unsigned int n);
     /** release the current layer and switches to another layer indexed by n.
     The current (old) layer will be removed from it's parent with 'cleanup:YES'.
     */
+    // 交换替代层，使用序号为参数
     void switchToAndReleaseMe(unsigned int n);
 };
 
