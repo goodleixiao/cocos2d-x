a4a17812532f85d649fd16a019319c09b2f8fdbc
erduoerduo <94069625@qq.com>
2013-03-16 16:26:36 +0800

    用户设置

--- cocos2dx/support/user_default/CCUserDefault.h
+++ cocos2dx/support/user_default/CCUserDefault.h
@@ -30,7 +30,7 @@ THE SOFTWARE.
 NS_CC_BEGIN
 
 /**
- * @addtogroup data_storage
+ * @addtogroup data_storage     数据储存
  * @{
  */
 
@@ -42,6 +42,8 @@ NS_CC_BEGIN
  * It supports the following base types:
  * bool, int, float, double, string
  */
+// 作为小的数据库。保存和获取基本类型的值。
+// 支持的类型有：bool, int, float, double, string
 class CC_DLL CCUserDefault
 {
 public:
@@ -53,30 +55,35 @@ public:
     @brief Get bool value by key, if the key doesn't exist, a default value will return.
      You can set the default value, or it is false.
     */
+    // 获取方法； 获取布尔值，用键；若不存在，返回默认值；可以设置默认值，否则返回false
     bool    getBoolForKey(const char* pKey);
     bool    getBoolForKey(const char* pKey, bool defaultValue);
     /**
     @brief Get integer value by key, if the key doesn't exist, a default value will return.
      You can set the default value, or it is 0.
     */
+    // 获取整数值
     int     getIntegerForKey(const char* pKey);
     int     getIntegerForKey(const char* pKey, int defaultValue);
     /**
     @brief Get float value by key, if the key doesn't exist, a default value will return.
      You can set the default value, or it is 0.0f.
     */
+    // 获取浮点数值
     float    getFloatForKey(const char* pKey);
     float    getFloatForKey(const char* pKey, float defaultValue);
     /**
     @brief Get double value by key, if the key doesn't exist, a default value will return.
      You can set the default value, or it is 0.0.
     */
+    // 获取double值
     double  getDoubleForKey(const char* pKey);
     double  getDoubleForKey(const char* pKey, double defaultValue);
     /**
     @brief Get string value by key, if the key doesn't exist, a default value will return.
     You can set the default value, or it is "".
     */
+    // 获取字符串的值
     std::string getStringForKey(const char* pKey);
     std::string getStringForKey(const char* pKey, const std::string & defaultValue);
 
@@ -85,26 +92,32 @@ public:
     /**
     @brief Set bool value by key.
     */
+    // 设置方法，设置布尔值，使用键为参数
     void    setBoolForKey(const char* pKey, bool value);
     /**
     @brief Set integer value by key.
     */
+    // 设置整数值
     void    setIntegerForKey(const char* pKey, int value);
     /**
     @brief Set float value by key.
     */
+    // 设置浮点值
     void    setFloatForKey(const char* pKey, float value);
     /**
     @brief Set double value by key.
     */
+    // 设置double值
     void    setDoubleForKey(const char* pKey, double value);
     /**
     @brief Set string value by key.
     */
+    // 设置字符串
     void    setStringForKey(const char* pKey, const std::string & value);
     /**
      @brief Save content to xml file
      */
+    // 保存方法，到xml文件中
     void    flush();
 
     static CCUserDefault* sharedUserDefault();
