5048a535feed9e91e5df3c8beb383113ebd49b29
erduoerduo <94069625@qq.com>
2013-03-16 09:50:25 +0800

    层信息，瓦片信息，地图信息

--- cocos2dx/tilemap_parallax_nodes/CCTMXXMLParser.h
+++ cocos2dx/tilemap_parallax_nodes/CCTMXXMLParser.h
@@ -48,7 +48,7 @@ class CCTMXObjectGroup;
 */
 
 /**
- * @addtogroup tilemap_parallax_nodes
+ * @addtogroup tilemap_parallax_nodes 瓦片地图 视差节点
  * @{
  */
 
@@ -86,6 +86,10 @@ typedef enum ccTMXTileFlags_ {
 
 This information is obtained from the TMX file.
 */
+/** 32比特全局瓦片id标示瓦片
+ * 层信息包括：层名称，层大小，层容量（运行时），层的可见性（若不可见则不创建）
+ * 可以从tmx文件获取
+ */
 class CC_DLL CCTMXLayerInfo : public CCObject
 {
     CC_PROPERTY(CCDictionary*, m_pProperties, Properties);
@@ -114,6 +118,10 @@ public:
 
 This information is obtained from the TMX file. 
 */
+/** tmx瓦片集合信息：包括
+ * 名称，容量，余量，瓦片大小，图片（瓦片使用），图片大小
+ * 可以从tmx文件中获取
+ */
 class CC_DLL CCTMXTilesetInfo : public CCObject
 {
 public:
@@ -123,8 +131,10 @@ public:
     unsigned int    m_uSpacing;
     unsigned int    m_uMargin;
     //! filename containing the tiles (should be spritesheet / texture atlas)
+    // 文件名称（需要是精灵表，纹理地图）
     std::string     m_sSourceImage;
     //! size in pixels of the image
+    //! 图片大小，以像素为单位
     CCSize          m_tImageSize;
 public:
     CCTMXTilesetInfo();
@@ -145,43 +155,67 @@ And it also contains:
 This information is obtained from the TMX file.
 
 */
+/** tmx地图信息包括：
+ * 方向：正交，六角，等距
+ * 瓦片大小
+ * 地图大小
+ * 还有：层（一组层信息对象），瓦片集合（一组瓦片信息对象),对象组（一组tmx对象组信息对象）
+ * 
+ * 可以从tmx文件获取这些信息
+ */
 class CC_DLL CCTMXMapInfo : public CCObject, public CCSAXDelegator
 {    
 public:    
     /// map orientation
+    /// 地图方向
     CC_SYNTHESIZE(int,    m_nOrientation, Orientation);
     /// map width & height
+    /// 地图宽，高
     CC_SYNTHESIZE_PASS_BY_REF(CCSize, m_tMapSize, MapSize);
     /// tiles width & height
+    /// 瓦片宽，高
     CC_SYNTHESIZE_PASS_BY_REF(CCSize, m_tTileSize, TileSize);
     /// Layers
+    /// 层
     CC_PROPERTY(CCArray*, m_pLayers, Layers);
     /// tilesets
+    /// 瓦片集合
     CC_PROPERTY(CCArray*, m_pTilesets, Tilesets);
     /// ObjectGroups
+    /// 对象组
     CC_PROPERTY(CCArray*, m_pObjectGroups, ObjectGroups);
     /// parent element
+    /// 父类元素
     CC_SYNTHESIZE(int, m_nParentElement, ParentElement);
     /// parent GID
+    /// 父类gid 
     CC_SYNTHESIZE(unsigned int, m_uParentGID, ParentGID);
     /// layer attribs
+    /// 层属性
     CC_SYNTHESIZE(int, m_nLayerAttribs, LayerAttribs);
     /// is storing characters?
+    /// 是否存储字符
     CC_SYNTHESIZE(bool, m_bStoringCharacters, StoringCharacters);
     /// properties
+    /// 属性
     CC_PROPERTY(CCDictionary*, m_pProperties, Properties);
 public:    
     CCTMXMapInfo();
     virtual ~CCTMXMapInfo();
     /** creates a TMX Format with a tmx file */
+    // 创建tmx格式，使用tmx文件为参数
     static CCTMXMapInfo * formatWithTMXFile(const char *tmxFile);
     /** creates a TMX Format with an XML string and a TMX resource path */
+    // 创建tmx格式，使用xml字符串，tmx资源路径为参数
     static CCTMXMapInfo * formatWithXML(const char* tmxString, const char* resourcePath);
     /** initializes a TMX format with a  tmx file */
+    // 初始化tmx格式，使用tmx文件为参数
     bool initWithTMXFile(const char *tmxFile);
     /** initializes a TMX format with an XML string and a TMX resource path */
+    // 初始化tmx格式，使用xml字符串，tmx资源路径为参数
     bool initWithXML(const char* tmxString, const char* resourcePath);
     /** initializes parsing of an XML file, either a tmx (Map) file or tsx (Tileset) file */
+    // 初始化方法，使用xml文件，tmx文件或瓦片集合文件为参数
     bool parseXMLFile(const char *xmlFilename);
     /* initializes parsing of an XML string, either a tmx (Map) string or tsx (Tileset) string */
     bool parseXMLString(const char *xmlString);
@@ -190,6 +224,7 @@ public:
     void setTileProperties(CCDictionary* tileProperties);
 
     // implement pure virtual methods of CCSAXDelegator
+    // 实现虚函数
     void startElement(void *ctx, const char *name, const char **atts);
     void endElement(void *ctx, const char *name);
     void textHandler(void *ctx, const char *ch, int len);
@@ -202,12 +237,16 @@ private:
     void internalInit(const char* tmxFileName, const char* resourcePath);
 protected:
     //! tmx filename
+    //! tmx 文件名称
     std::string m_sTMXFileName;
     // tmx resource path
+    // tmx 资源路径
     std::string m_sResources;
     //! current string
+    //! 当前字符串
     std::string m_sCurrentString;
     //! tile properties
+    //! 瓦片属性
     CCDictionary* m_pTileProperties;
     unsigned int m_uCurrentFirstGID;
 };
